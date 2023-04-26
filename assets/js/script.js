0cd6f86050d315d69529088704d994356f8c1903
Affan <affanmomin14@gmail.com>
2023-04-26 18:33:41 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    setTimeout(function() {
+      var loader = document.getElementById("loader");
+      loader.style.display = "none";
+    }, 2000); // Delay for 3 seconds (3000 milliseconds)
+  }
+  
